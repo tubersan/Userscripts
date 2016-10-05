961f5f2029e565884073a0fb068b5c9869c7784f
Eric Gross <egross73@gmail.com>
2016-10-04 22:41:53 -0400

    initial commit
    This isn't completed... It works, but it doesn't include the main
    "feature" I want, linking to deleted posts. The problem is that I can't
    figure out a good way to determine what page the post would be on, so I
    can't actually create a link to it. I could probably create cookies, and
    a reload a bunch of times... but I won't do that.... this is a long
    description...

---
+++ AnimeBytes/AddForumLogLinks.user.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name        AnimeBytes Link Forum Log
+// @namespace   mukti.moe
+// @author      mukti
+// @description Add links to threads and posts from the forum log page.
+// @include     *animebytes.tv/log.php?type=forum*
+// @include     *animebyt.es/log.php?type=forum*
+// @version     1
+// @icon        http://animebytes.tv/favicon.ico
+// ==/UserScript==
+
+
+var fltables = document.getElementsByTagName('table');
+var forumlog = fltables[fltables.length - 1];
+var tpre = /.*(Thread|Post|Posts)\s#(\d+,?)+\s(\(Thread\s(\d+)\))?.*/;
+
+for (var i = 0, row; row = forumlog.rows[i]; i++) {
+
+	for (var j = 0, cell; cell = row.cells[j]; j++) {
+
+		if(cell.innerText.match(tpre)) {
+			
+			var matches = tpre.exec(cell.innerText);
+			
+			if(matches[1].match(/Thread/)) {
+				
+				var strreplace = matches[1] + " #" + matches[2];
+				cell.innerHTML = cell.innerText.replace(strreplace, "<a href='https://animebytes.tv/forums.php?action=viewthread&threadid=" + matches[2] + "'>" + strreplace + "</a>");
+				
+			}
+			
+			if(matches[1].match(/Post/)) {
+				
+				// Eventually loop through multiple posts in a log entry... once you figure out how to determin page...
+				//if(matches[1] == 'Posts') {
+				//	
+				//	var posts = matches[2].split(",");
+				//	
+				//}
+				
+				var strreplace = "Thread " + matches[4];
+				cell.innerHTML = cell.innerText.replace(strreplace, "<a href='https://animebytes.tv/forums.php?action=viewthread&threadid=" + matches[4] + "'>" + strreplace + "</a>");
+				var test = cell;
+				
+			}
+			
+		}
+	
+	}
+
+}
