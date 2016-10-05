f100b9117689fc08368b93a6ddcc89075b6b7343
Eric Gross <egross73@gmail.com>
2016-10-05 12:02:53 -0400

    Added null matches condition
    if statement to append to the URL if no matches are found in the current
    URL

--- AnimeBytes/ShowDeletedLink.user.js
+++ AnimeBytes/ShowDeletedLink.user.js
@@ -5,7 +5,7 @@
 // @description Add a button to append "&showdeleted=1" to all forum post URLs
 // @include     *animebytes.tv/forums.php*&threadid=*
 // @include     *animebyt.es/forums.php*&threadid=*
-// @version     1.2
+// @version     1.3
 // @icon        http://animebytes.tv/favicon.ico
 // ==/UserScript==
 
@@ -13,7 +13,15 @@ var lnk = document.createElement('a');
 var cururl = window.location.href;
 ptre = /(.*)(#post\d+)/;
 var matches = ptre.exec(cururl);
-lnk.href = matches[1] + '&showdeleted=1' + matches[2];
+if(matches) {
+	
+	lnk.href = matches[1] + '&showdeleted=1' + matches[2];
+	
+} else {
+	
+	lnk.href = cururl + '&showdeleted=1'
+	
+}
 lnk.appendChild(document.createTextNode('[Show Deleted Posts]'));
 linkbox = document.getElementsByClassName('linkbox')[0];
 linkbox.appendChild(lnk);
